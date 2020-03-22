c279ddece786b5947557899798ae218401195653
domainio <omega@omegas-MacBook-Pro.local>
2020-03-22 22:53:12 +0200

    netlify id widget

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget';
+netlifyIdentity.init({})
 export default props => (
   <Container>
     <Flex sx={{ flexDirection: "column", padding: 3 }}>
@@ -7,7 +9,7 @@ export default props => (
     <Button
       sx={{ marginTop: 2 }}
       onClick={() => {
-        alert("click");
+        netlifyIdentity.open();
       }}
     >Log In</Button>
     </Flex>
