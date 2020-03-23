577a4c9a5046ec6c66dbd87d4b3ede71bd86dd32
domainio <omega@omegas-MacBook-Pro.local>
2020-03-23 15:06:43 +0200

    test3

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui';
 import netlifyIdentity from 'netlify-identity-widget';
-netlifyIdentity.init({})
-export default props => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-    <Heading as="h1">Extend your Skills</Heading>
-    <Button
-      sx={{ marginTop: 2 }}
-      onClick={() => {
-        // netlifyIdentity.open();
-        alert("click");
-      }}
-    >Log In</Button>
-    </Flex>
-  </Container>
-)
+
+const Root = (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  })
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Extend your Skills</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            // netlifyIdentity.open();
+            alert("click");
+          }}
+        >Log In</Button>
+      </Flex>
+    </Container>
+  )
+}
+export default Root;
